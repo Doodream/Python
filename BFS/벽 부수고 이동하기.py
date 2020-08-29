87af3556df5fb208d1420a5ced2575dd1852f6fa
doodream <rhomo17@naver.com>
2020-08-29 23:47:07 +0900

    Review BFS question

--- BFS/벽 부수고 이동하기.py
+++ BFS/벽 부수고 이동하기.py
@@ -13,56 +13,40 @@
 # 출력
 # 첫째 줄에 최단 거리를 출력한다. 불가능할 때는 -1을 출력한다.
 from collections import deque
+
 N, M = map(int, input().split())
-graph = []
-# 3차원 배열의 [0][0] 부분은 벽을 부순적이 있는가 없는가? 를 추가로 나타내기 위해서
-visited = [[[0]*2 for _ in range(M)] for _ in range(N)]
+graph = [list(map(int, input())) for _ in range(N)]
+distance = [[[0]*2 for _ in range(M)] for _ in range(N)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
-for i in range(N):
-    graph.append(list(map(int, input())))
-
 
 def bfs(x, y, status):
     q = deque()
     q.append((x, y, status))
-    visited[x][y][status] = 1
+    distance[x][y][status] = 1
+
     while q:
-        nx, ny, nstatus = q.popleft()
+        cx, cy, status = q.popleft()
         for i in range(4):
-            xx, yy = nx + dx[i], ny + dy[i]
-            # 범위가 넘어가지 않았다면
-            if 0 <= xx < N and 0 <= yy < M:
-                # 방문한 적이 없고 벽이 없는 곳이면
-                if visited[xx][yy][nstatus] == 0 and graph[xx][yy] == 0:
-                    # 이동 횟수 누적
-                    visited[xx][yy][nstatus] = visited[nx][ny][nstatus] + 1
-                    q.append((xx, yy, nstatus))
-                # 현재 까지 벽이 한번도 부숴진 적이 없다면
-                if nstatus == 0:
-                # 현재까지 방문한 적이 없고 벽이 있다면
-                    if visited[xx][yy][nstatus + 1] == 0 and graph[xx][yy] == 1:
-                        # 벽이 부숴진 벽에 최단거리 입력하여 부순 흔적 체크와 최단거리 입력
-                        visited[xx][yy][nstatus + 1] = visited[nx][ny][nstatus] + 1
-                        # 이후로는 벽이 부숴진 흔적을 큐로 남겨야 하므로 nstatus + 1을 다음 큐에 입력한다.
-                        q.append((xx, yy, nstatus + 1))
+            nx, ny = cx + dx[i], cy + dy[i]
+            if 0 <= nx < N and 0 <= ny < M:
+                if graph[nx][ny] == 0 and distance[nx][ny][status] == 0:
+                    distance[nx][ny][status] = distance[cx][cy][status] + 1
+                    q.append((nx, ny, status))
+                # 현재 이미 벽을 부수지 않은 상태이므로 당연히 distance[nx][ny][status + 1] == 0 이다.
+                if status == 0:
+                    if graph[nx][ny] == 1:
+                        distance[nx][ny][status + 1] = distance[cx][cy][status] + 1
+                        q.append((nx, ny, status + 1))
 
 
 bfs(0, 0, 0)
-# 벽을 부수기 전의 값도 있고 벽을 부순 후의 값도 있다면 둘중 작은 것을 출력
-# (벽을 부숴도 갈 수 있고, 굳이 부수지 않더라도 도달 할 수 있다)
-if visited[N - 1][M - 1][0] != 0 and visited[N - 1][M - 1][1] != 0:
-    print(min(visited[N - 1][M - 1][0], visited[N - 1][M - 1][1]))
-# 벽을 부수기 전의 값만 있다(벽을 부수면 도달이 불가능한 경우)
-elif visited[N - 1][M - 1][0] != 0:
-    print(visited[N - 1][M - 1][0])
-# 벽을 부순 후의 값만 있다.(벽을 부숴야지만 도달 가능한 경우)
-elif visited[N - 1][M - 1][1] != 0:
-    print(visited[N - 1][M - 1][1])
-# 둘다 값이 없다면 애초에 도달이 아얘 불가능한 경우
+
+if distance[N - 1][M - 1][0] != 0 and distance[N - 1][M - 1][1] != 0:
+    print(min(distance[N - 1][M - 1]))
+elif distance[N - 1][M - 1][0] != 0:
+    print(distance[N - 1][M - 1][0])
+elif distance[N - 1][M - 1][1] != 0:
+    print(distance[N - 1][M - 1][1])
 else:
     print(-1)
-
-
-
